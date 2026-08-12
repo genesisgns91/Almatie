281164c745e8895e5203d078674bccc5e3157f1a
genesisgns91 <genesisgns@gmail.com>
2026-08-12 08:42:45 -0300

    Update Firebase configuration and imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Substitua com as suas chaves do Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SEU_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyAtmaLD5d_2px1YJ251V_pDOBuJk0O2yOg",
+  authDomain: "almatie.firebaseapp.com",
+  projectId: "almatie",
+  storageBucket: "almatie.firebasestorage.app",
+  messagingSenderId: "1011119969194",
+  appId: "1:1011119969194:web:251c0e8351ffa4875e11d3",
+  measurementId: "G-5F0XQ3Z0PV"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
